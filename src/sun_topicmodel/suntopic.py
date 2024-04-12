19a9296a68f36d7563371a8edcaf787459b87459
TillRS <till.saenger@gmail.com>
2024-04-12 10:03:40 -0400

    pre-commit formatted

--- src/sun_topicmodel/suntopic.py
+++ src/sun_topicmodel/suntopic.py
@@ -5,25 +5,23 @@ SUN Topic Model
 
 Authors: Till R. Saenger, ORFE Princeton
 
-[1] Saenger, Hinck, Grimmer, and Stewart. AutoPersuade: 
+[1] Saenger, Hinck, Grimmer, and Stewart. AutoPersuade:
 
 """
 
 from __future__ import annotations
 
-import numpy as np
-import multiprocessing
 import logging
 import logging.config
-from logging.handlers import QueueHandler
+
 import matplotlib.pyplot as plt
-from matplotlib import rc
-from sklearn.model_selection import KFold
-from sklearn.metrics import mean_squared_error
+import numpy as np
 from joblib import Parallel, delayed
+from matplotlib import rc
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+
 from sun_topicmodel.snmf import SNMF
-
-
 
 
 class suntopic:
@@ -41,16 +39,23 @@ class suntopic:
         """
 
         if alpha < 0 or alpha > 1:
-            raise ValueError("Alpha must be in [0,1]")
+            msg = "Alpha must be in [0,1]"
+            raise ValueError(msg)
         if num_bases < 1:
-            raise ValueError("Number of bases must be at least 1")
+            msg = "Number of bases must be at least 1"
+            raise ValueError(msg)
         if num_bases > X.shape[1]:
-            raise ValueError("Number of bases must be less than the dimensionality of X.shape[1]")
+            msg = "Number of bases must be less than the dimensionality of X.shape[1]"
+            raise ValueError(
+                msg
+            )
         if num_bases != int(num_bases):
-            raise ValueError("Number of bases must be an integer")
+            msg = "Number of bases must be an integer"
+            raise ValueError(msg)
 
         if len(Y) != X.shape[0]:
-            raise ValueError("Y and X must have the same number of samples")
+            msg = "Y and X must have the same number of samples"
+            raise ValueError(msg)
 
         def setup_logging():
             self._logger = logging.getLogger("suntopic")
@@ -62,66 +67,65 @@ class suntopic:
             # Add formatter to handler
             ch.setFormatter(formatter)
             # Add handler to logger
-            self._logger.addHandler(ch) 
-        
-        setup_logging()        
+            self._logger.addHandler(ch)
+
+        setup_logging()
 
         self.Y = Y
         self.X = X
         self.num_bases = num_bases
         self.alpha = alpha
 
-        data = np.hstack((np.sqrt(alpha) * X, np.sqrt(1 - alpha) * np.array(Y).reshape(-1, 1)))
+        data = np.hstack(
+            (np.sqrt(alpha) * X, np.sqrt(1 - alpha) * np.array(Y).reshape(-1, 1))
+        )
         self.data = data
         self.model = SNMF(data, num_bases)
         self.model.random_state = random_state
-    
 
-
-    def fit(self,
-            niter=100,
-            verbose=False,
-            compute_w=True,
-            compute_h=True,
-            compute_err=True):
+    def fit(
+        self, niter=100, verbose=False, compute_w=True, compute_h=True, compute_err=True
+    ):
         """
         Fit the suntopic model to the data.
         """
-        self.model.factorize(niter=niter,
-                                verbose=verbose,
-                                compute_w=compute_w,
-                                compute_h=compute_h,
-                                compute_err=compute_err)
+        self.model.factorize(
+            niter=niter,
+            verbose=verbose,
+            compute_w=compute_w,
+            compute_h=compute_h,
+            compute_err=compute_err,
+        )
 
-    def predict(self, 
-                X_new,
-                return_topics=False,
-                niter=100,
-                random_state=None,
-                verbose=False,
-                compute_err=True):
+    def predict(
+        self,
+        X_new,
+        return_topics=False,
+        niter=100,
+        random_state=None,
+        verbose=False,
+        compute_err=True,
+    ):
         """
         Predict the response variable for new data X_new.
         """
         if X_new.shape[1] != self.X.shape[1]:
-            raise ValueError("X_new.shape[1] must be equal to X.shape[1]")
-        
+            msg = "X_new.shape[1] must be equal to X.shape[1]"
+            raise ValueError(msg)
+
         if not hasattr(self.model, "H"):
-            raise ValueError("Model has not been fitted yet. Call fit() first.")
-        
+            msg = "Model has not been fitted yet. Call fit() first."
+            raise ValueError(msg)
+
         data_new = np.sqrt(self.alpha) * X_new
-        self._model_pred = SNMF(data_new, 
-                                self.num_bases,
-                                random_state=random_state)
-        self._model_pred.H = self.model.H[:,:-1]
+        self._model_pred = SNMF(data_new, self.num_bases, random_state=random_state)
+        self._model_pred.H = self.model.H[:, :-1]
 
         self._model_pred.factorize(
-                                niter=niter,
-                                verbose=verbose,
-                                compute_h=False,
-                                compute_err=compute_err)
-        
-        Y_pred = np.dot(self._model_pred.W, self.model.H[:,-1])
+            niter=niter, verbose=verbose, compute_h=False, compute_err=compute_err
+        )
+
+        Y_pred = np.dot(self._model_pred.W, self.model.H[:, -1])
         Y_pred /= np.sqrt(1 - self.alpha)
         if return_topics is False:
             return Y_pred
@@ -139,40 +143,43 @@ class suntopic:
         Get the coefficients from the suntopic model.
         """
         return self.model.H
-    
+
     def summary(self):
         """
         Print a summary of the suntopic model.
         """
 
         if not hasattr(self.model, "H"):
-            raise ValueError("Model has not been fitted yet. Call fit() first.")
+            msg = "Model has not been fitted yet. Call fit() first."
+            raise ValueError(msg)
 
         print("Suntopic Model Summary")
-        print("="*50)
+        print("=" * 50)
         print("Number of topics: ", self.num_bases)
         print("Alpha: ", self.alpha)
         print("Data shape: ", self.data.shape)
         print("Model: ", self.model)
         print("Random initialization state: ", self.model.random_state)
         # print("Frobenius norm error: ", self.model.ferr)
-        print("Prediction coefficients: ", self.model.H[:,-1])
+        print("Prediction coefficients: ", self.model.H[:, -1])
         # print("Topics: ", self.model.W)
         # print("Coefficients: ", self.model.H)
-        
-        return None
+
+        return
 
     def save(self, filename):
         """
         Save the suntopic model to a file.
         """
-        np.savez(filename, 
-                 Y = self.Y, 
-                 X = self.X, 
-                 W = self.model.W, 
-                 H = self.model.H, 
-                 alpha = self.alpha, 
-                 random_state = str(self.model.random_state))
+        np.savez(
+            filename,
+            Y=self.Y,
+            X=self.X,
+            W=self.model.W,
+            H=self.model.H,
+            alpha=self.alpha,
+            random_state=str(self.model.random_state),
+        )
 
     @staticmethod
     def load(filename):
@@ -180,85 +187,124 @@ class suntopic:
         Load a suntopic model from a file.
         """
         npzfile = np.load(filename)
-        
+
         if npzfile["random_state"] == "None":
-            loaded_model = suntopic(Y=npzfile["Y"], 
-                                X=npzfile["X"], 
-                                alpha=npzfile["alpha"], 
-                                num_bases=npzfile["W"].shape[1])
+            loaded_model = suntopic(
+                Y=npzfile["Y"],
+                X=npzfile["X"],
+                alpha=npzfile["alpha"],
+                num_bases=npzfile["W"].shape[1],
+            )
         else:
-            loaded_model = suntopic(Y=npzfile["Y"], 
-                                X=npzfile["X"], 
-                                alpha=npzfile["alpha"], 
-                                num_bases=npzfile["W"].shape[1],
-                                random_state=int(npzfile["random_state"]))
-            
+            loaded_model = suntopic(
+                Y=npzfile["Y"],
+                X=npzfile["X"],
+                alpha=npzfile["alpha"],
+                num_bases=npzfile["W"].shape[1],
+                random_state=int(npzfile["random_state"]),
+            )
+
         loaded_model.model.W = npzfile["W"]
         loaded_model.model.H = npzfile["H"]
         return loaded_model
-    
-    def hyperparam_cv(self, alpha_range, num_bases_range, 
-                      cv_folds, random_state = None, parallel = False, verbose = False,
-                      niter=100):
+
+    def hyperparam_cv(
+        self,
+        alpha_range,
+        num_bases_range,
+        cv_folds,
+        random_state=None,
+        parallel=False,
+        verbose=False,
+        niter=100,
+    ):
         """
         Calculate the cross-validated mean squared error for different values of alpha and num_bases.
         """
         for alpha in alpha_range:
             if alpha < 0 or alpha > 1:
-                raise ValueError("Alpha must be in [0,1]")
-            
+                msg = "Alpha must be in [0,1]"
+                raise ValueError(msg)
+
         for num_bases in num_bases_range:
             if num_bases < 1:
-                raise ValueError("Each number of bases must be at least 1")
+                msg = "Each number of bases must be at least 1"
+                raise ValueError(msg)
             if num_bases > self.X.shape[1]:
-                raise ValueError("Each number of bases must be less than the dimensionality of X.shape[1]")
+                msg = "Each number of bases must be less than the dimensionality of X.shape[1]"
+                raise ValueError(
+                    msg
+                )
             if num_bases != int(num_bases):
-                raise ValueError("Each number of bases must be an integer")
-        
+                msg = "Each number of bases must be an integer"
+                raise ValueError(msg)
+
         if cv_folds < 2:
-            raise ValueError("Number of folds must be at least 2")
+            msg = "Number of folds must be at least 2"
+            raise ValueError(msg)
         if cv_folds != int(cv_folds):
-            raise ValueError("Number of folds must be an integer")
+            msg = "Number of folds must be an integer"
+            raise ValueError(msg)
         if cv_folds > len(self.Y):
-            raise ValueError("Number of folds must be less than the number of samples")
-        
+            msg = "Number of folds must be less than the number of samples"
+            raise ValueError(msg)
+
         if verbose:
             self._logger.setLevel(logging.INFO)
         else:
             self._logger.setLevel(logging.ERROR)
-        
+
         self.cv_alpha_range = alpha_range
         self.cv_num_base_range = num_bases_range
         self.cv_folds = cv_folds
-        self.cv_errors = np.full((len(alpha_range), len(num_bases_range), cv_folds), None)
+        self.cv_errors = np.full(
+            (len(alpha_range), len(num_bases_range), cv_folds), None
+        )
         self.cv_random_state = random_state
-        
+
         kf = KFold(n_splits=cv_folds, random_state=self.cv_random_state, shuffle=True)
-        self.cv_errors = np.ones((len(num_bases_range), len(alpha_range), cv_folds))*np.nan
+        self.cv_errors = (
+            np.ones((len(num_bases_range), len(alpha_range), cv_folds)) * np.nan
+        )
 
-
-        def predict_Y_mse(self, k, alpha, num_bases, train_index, test_index, random_state=self.cv_random_state):
-            model = suntopic(Y=self.Y[train_index], 
-                            X=self.X[train_index], 
-                            alpha=alpha, 
-                            num_bases=num_bases, 
-                            random_state=random_state)
+        def predict_Y_mse(
+            self,
+            k,
+            alpha,
+            num_bases,
+            train_index,
+            test_index,
+            random_state=self.cv_random_state,
+        ):
+            model = suntopic(
+                Y=self.Y[train_index],
+                X=self.X[train_index],
+                alpha=alpha,
+                num_bases=num_bases,
+                random_state=random_state,
+            )
             model.fit(niter=niter, verbose=False)
             Y_pred = model.predict(self.X[test_index], random_state=random_state)
             mse = mean_squared_error(self.Y[test_index], Y_pred)
-            self._logger.info(f"Alpha: {alpha}, Num bases: {num_bases}, Fold: {k}, MSE: {mse}") # currently does not work for parallel
+            self._logger.info(
+                f"Alpha: {alpha}, Num bases: {num_bases}, Fold: {k}, MSE: {mse}"
+            )  # currently does not work for parallel
             return mse
 
-        if parallel == False:
+        if parallel is False:
             for i, num_bases in enumerate(num_bases_range):
                 for j, alpha in enumerate(alpha_range):
                     for k, (train_index, test_index) in enumerate(kf.split(self.Y)):
-                        self.cv_errors[i,j,k]= predict_Y_mse(self, k, alpha, num_bases, train_index, test_index)
-        else: 
+                        self.cv_errors[i, j, k] = predict_Y_mse(
+                            self, k, alpha, num_bases, train_index, test_index
+                        )
+        else:
             # Sequentially loop over alpha_ranges and parallelize across topic_range
-            num_cores = -1 if len(alpha_range) > 1 else 1 # Use all available cores
+            num_cores = -1 if len(alpha_range) > 1 else 1  # Use all available cores
             results = Parallel(n_jobs=num_cores)(
-                delayed(predict_Y_mse)(self, k, alpha, num_bases, train_index, test_index)
+                delayed(predict_Y_mse)(
+                    self, k, alpha, num_bases, train_index, test_index
+                )
                 for i, num_bases in enumerate(num_bases_range)
                 for j, alpha in enumerate(alpha_range)
                 for k, (train_index, test_index) in enumerate(kf.split(self.Y))
@@ -269,7 +315,7 @@ class suntopic:
                 for j in range(len(alpha_range)):
                     for k in range(cv_folds):
                         self.cv_errors[i, j, k] = results[idx]
-                        idx += 1    
+                        idx += 1
 
     def cv_summary(self, top_hyperparam_combinations=3):
         """
@@ -277,37 +323,58 @@ class suntopic:
         """
 
         if hasattr(self, "cv_errors") is False:
-            raise ValueError("Cross-validation errors have not been computed yet. Call hyperparam_cv() first.")
+            msg = "Cross-validation errors have not been computed yet. Call hyperparam_cv() first."
+            raise ValueError(
+                msg
+            )
 
         mean_cv_errors = np.mean(self.cv_errors, axis=2)
-        min_idx = np.unravel_index(np.argsort(mean_cv_errors, axis=None), mean_cv_errors.shape)
+        min_idx = np.unravel_index(
+            np.argsort(mean_cv_errors, axis=None), mean_cv_errors.shape
+        )
 
         print("Cross-Validation Summary")
-        print("="*50)
+        print("=" * 50)
         print("Alpha candidate values: ", self.cv_alpha_range)
         print("Number of topics: ", self.cv_num_base_range)
         print("Number of folds: ", self.cv_folds)
         print("CV Random state: ", self.cv_random_state)
-        print("="*50)
+        print("=" * 50)
         for i in range(top_hyperparam_combinations):
-            print(f"Top {i+1} hyperparam combinations - num_bases: {self.cv_num_base_range[min_idx[0][i]]:.2f}, alpha: {self.cv_alpha_range[min_idx[1][i]]:.2f}, MSE: {mean_cv_errors[min_idx[0][i], min_idx[1][i]]:.4f}")
-        return None
-    
-    def cv_mse_plot(self, figsize=(10, 6), title="Cross-Validation MSE", return_plot=False, benchmark=None):
+            print(
+                f"Top {i+1} hyperparam combinations - num_bases: {self.cv_num_base_range[min_idx[0][i]]:.2f}, alpha: {self.cv_alpha_range[min_idx[1][i]]:.2f}, MSE: {mean_cv_errors[min_idx[0][i], min_idx[1][i]]:.4f}"
+            )
+        return
+
+    def cv_mse_plot(
+        self,
+        figsize=(10, 6),
+        title="Cross-Validation MSE",
+        return_plot=False,
+        benchmark=None,
+    ):
         """
         Return plot of cross-validation errors.
         """
         if hasattr(self, "cv_errors") is False:
-            raise ValueError("Cross-validation errors have not been computed yet. Call hyperparam_cv() first.")
-        
-        rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
-        plt.rcParams['text.usetex'] = True
+            msg = "Cross-validation errors have not been computed yet. Call hyperparam_cv() first."
+            raise ValueError(
+                msg
+            )
+
+        rc("font", **{"family": "serif", "serif": ["Computer Modern"]})
+        plt.rcParams["text.usetex"] = True
 
         mean_cv_errors = np.mean(self.cv_errors, axis=2)
 
         fig, ax = plt.subplots(figsize=figsize)
         for i, num_bases in enumerate(self.cv_num_base_range):
-            ax.plot(self.cv_alpha_range, mean_cv_errors[i], label=f"{num_bases} topics", marker="o")
+            ax.plot(
+                self.cv_alpha_range,
+                mean_cv_errors[i],
+                label=f"{num_bases} topics",
+                marker="o",
+            )
         ax.set_xlabel(r"$\alpha$")
         ax.set_ylabel("MSE")
         ax.set_title(title)
@@ -320,9 +387,3 @@ class suntopic:
         else:
             plt.show()
             return None
-
-
-
-
-
-
--- tests/test_suntopic.py
+++ tests/test_suntopic.py
@@ -10,23 +10,34 @@ from sun_topicmodel import suntopic
 sample_size = 10
 data_dim = 20
 
+
 @pytest.fixture()
 def sample_X():
     return np.random.rand(sample_size, data_dim)
+
 
 @pytest.fixture()
 def sample_Y():
     return np.random.rand(sample_size)
 
-def test_initialization(sample_X, sample_Y, alpha = 0.5, num_bases = 5):
+
+def test_initialization(sample_X, sample_Y, alpha=0.5, num_bases=5):
     # Test initialization of suntopic instance
-    model = suntopic(sample_Y, sample_X, alpha= alpha, num_bases=num_bases)
-    assert np.allclose(model.data, np.hstack((np.sqrt(alpha) * sample_X, 
-                                          np.sqrt(1 - alpha) * np.array(sample_Y).reshape(-1, 1))))
+    model = suntopic(sample_Y, sample_X, alpha=alpha, num_bases=num_bases)
+    assert np.allclose(
+        model.data,
+        np.hstack(
+            (
+                np.sqrt(alpha) * sample_X,
+                np.sqrt(1 - alpha) * np.array(sample_Y).reshape(-1, 1),
+            )
+        ),
+    )
     assert model.num_bases == 5
     assert model.alpha == 0.5
     assert model.Y.shape == sample_Y.shape
     assert model.X.shape == sample_X.shape
+
 
 @pytest.mark.parametrize("alpha", [-0.1, 2])
 def test_initialization_with_invalid_alpha(sample_X, sample_Y, alpha):
@@ -34,30 +45,36 @@ def test_initialization_with_invalid_alpha(sample_X, sample_Y, alpha):
     with pytest.raises(ValueError):
         suntopic(sample_Y, sample_X, alpha=alpha, num_bases=5)
 
+
 @pytest.mark.parametrize("num_bases", [0, 21, 10.5])
 def test_initialization_with_invalid_num_bases(sample_X, sample_Y, num_bases):
     # Test initialization of suntopic instance with invalid num_bases
     with pytest.raises(ValueError):
         suntopic(sample_Y, sample_X, alpha=0.5, num_bases=num_bases)
 
+
 def test_initialization_with_invalid_num_samples(sample_X, sample_Y):
     # Test initialization of suntopic instance with invalid num_samples
     with pytest.raises(ValueError):
-        suntopic(sample_Y, sample_X[:5], alpha= 0.5, num_bases=5)   
+        suntopic(sample_Y, sample_X[:5], alpha=0.5, num_bases=5)
+
 
 def test_fit(sample_X, sample_Y):
     # Test fit method of suntopic instance
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     model.fit(niter=10)
     assert model.model.W.shape == (sample_size, 4)
     assert model.model.H.shape == (4, data_dim + 1)
     assert (model.get_coefficients() == model.model.H).all()
     assert (model.get_topics() == model.model.W).all()
 
-@pytest.mark.parametrize("random_state", [None,21])
+
+@pytest.mark.parametrize("random_state", [None, 21])
 def test_save_load(sample_X, sample_Y, random_state):
     # Test save and load methods of suntopic instance
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4, random_state=random_state)
+    model = suntopic(
+        sample_Y, sample_X, alpha=0.5, num_bases=4, random_state=random_state
+    )
     model.fit(niter=10)
     model.save(filename="test_model.npz")
     loaded_model = suntopic.load(filename="test_model.npz")
@@ -68,35 +85,39 @@ def test_save_load(sample_X, sample_Y, random_state):
     assert np.allclose(model.Y, loaded_model.Y)
     assert np.allclose(model.X, loaded_model.X)
 
+
 def test_predict_and_infer(sample_X, sample_Y):
     # Test predict method of suntopic instance
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     model.fit(niter=10)
     X_new = np.random.rand(5, data_dim)
     Y_pred = model.predict(X_new)
     assert Y_pred.shape == (5,)
     Y_pred, W_pred = model.predict(X_new, return_topics=True)
     assert Y_pred.shape == (5,)
-    assert W_pred.shape == (5,4)
+    assert W_pred.shape == (5, 4)
+
 
 def test_predict_with_invalid_X_new(sample_X, sample_Y):
     # Test predict method of suntopic instance with invalid X_new
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     model.fit(niter=10)
     X_new = np.random.rand(5, 21)
     with pytest.raises(ValueError):
         model.predict(X_new)
 
+
 def test_predict_without_fit(sample_X, sample_Y):
     # Test predict method of suntopic instance without fit
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     X_new = np.random.rand(5, 20)
     with pytest.raises(ValueError):
         model.predict(X_new)
 
+
 def test_predict_with_invalid_return_topics(sample_X, sample_Y):
     # Test predict method of suntopic instance with invalid return_topics
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     model.fit(niter=10)
     X_new = np.random.rand(5, 22)
     with pytest.raises(ValueError):
@@ -105,52 +126,74 @@ def test_predict_with_invalid_return_topics(sample_X, sample_Y):
 
 def test_summary(sample_X, sample_Y):
     # Test summary method of suntopic instance
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     model.fit(niter=10)
     assert model.summary() is None
 
+
 def test_summary_without_fit(sample_X, sample_Y):
     # Test summary method of suntopic instance without fit
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     with pytest.raises(ValueError):
         model.summary()
+
 
 def test_hyperparam_cv():
     Y = np.random.rand(100)
     X = np.random.rand(100, 200)
     # Test find_alpha_cv method of suntopic instance
-    model = suntopic(Y, X, alpha= 0.5, num_bases=4)
-    model.hyperparam_cv(alpha_range=[0.1, 0.5, 0.9], num_bases_range=[2, 4, 6], cv_folds=3, random_state=21, niter=10)
+    model = suntopic(Y, X, alpha=0.5, num_bases=4)
+    model.hyperparam_cv(
+        alpha_range=[0.1, 0.5, 0.9],
+        num_bases_range=[2, 4, 6],
+        cv_folds=3,
+        random_state=21,
+        niter=10,
+    )
     assert model.cv_errors.shape == (3, 3, 3)
     assert model.cv_alpha_range == [0.1, 0.5, 0.9]
     assert model.cv_num_base_range == [2, 4, 6]
     assert model.cv_folds == 3
-    assert ((0<= model.cv_errors) & (model.cv_errors <= 1)).all()
+    assert ((model.cv_errors >= 0) & (model.cv_errors <= 1)).all()
     cv_errors = model.cv_errors
 
-    model_parallel = suntopic(Y, X, alpha= 0.5, num_bases=4)
-    model_parallel.hyperparam_cv(alpha_range=[0.1, 0.5, 0.9], num_bases_range=[2, 4, 6], cv_folds=3, random_state=21, niter=10, parallel=True)
+    model_parallel = suntopic(Y, X, alpha=0.5, num_bases=4)
+    model_parallel.hyperparam_cv(
+        alpha_range=[0.1, 0.5, 0.9],
+        num_bases_range=[2, 4, 6],
+        cv_folds=3,
+        random_state=21,
+        niter=10,
+        parallel=True,
+    )
     assert model_parallel.cv_errors.shape == (3, 3, 3)
     assert model_parallel.cv_alpha_range == [0.1, 0.5, 0.9]
     assert model_parallel.cv_num_base_range == [2, 4, 6]
     assert model_parallel.cv_folds == 3
-    assert ((0<= model_parallel.cv_errors) & (model_parallel.cv_errors <= 1)).all()
+    assert ((model_parallel.cv_errors >= 0) & (model_parallel.cv_errors <= 1)).all()
     assert cv_errors.all() == model_parallel.cv_errors.all()
 
 
 def test_cv_summary_without_fit(sample_X, sample_Y):
     # Test summary method of suntopic instance without fit
-    model = suntopic(sample_Y, sample_X, alpha= 0.5, num_bases=4)
+    model = suntopic(sample_Y, sample_X, alpha=0.5, num_bases=4)
     with pytest.raises(ValueError):
-        model.cv_summary() 
+        model.cv_summary()
+
 
 def test_cv_mse_plot():
     # Test cv_mse_plot method of suntopic instance
     Y = np.random.rand(100)
     X = np.random.rand(100, 200)
-    model = suntopic(Y, X, alpha= 0.5, num_bases=4)
+    model = suntopic(Y, X, alpha=0.5, num_bases=4)
     with pytest.raises(ValueError):
         model.cv_mse_plot()
-    model.hyperparam_cv(alpha_range=[0.1, 0.5, 0.9], num_bases_range=[2, 4, 6], cv_folds=3, random_state=21, niter=10)
-    fig = model.cv_mse_plot(return_plot=True) 
+    model.hyperparam_cv(
+        alpha_range=[0.1, 0.5, 0.9],
+        num_bases_range=[2, 4, 6],
+        cv_folds=3,
+        random_state=21,
+        niter=10,
+    )
+    fig = model.cv_mse_plot(return_plot=True)
     assert fig is not None
