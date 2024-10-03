9e4befd0b13f1137cee88c10030e85bd4f0bf966
TillRS <till.saenger@gmail.com>
2024-10-03 13:16:36 -0400

    updated test to work with NotImplementedErrors

--- tests/test_base.py
+++ tests/test_base.py
@@ -17,20 +17,5 @@ def sample_data():
 # Define generic test cases
 def test_initialization(sample_data):
     # Test initialization of PyMFBase instance
-    model = PyMFBase(sample_data, num_bases=5)
-    assert model.data.shape == sample_data.shape
-    assert model._num_bases == 5
-    assert model._data_dimension == 20
-    assert model._num_samples == 10
-
-
-def test_initialization_with_random_state(sample_data):
-    # Test initialization of PyMFBase instance with random_state
-    model = PyMFBase(sample_data, num_bases=4, random_state=42)
-    assert model.random_state == 42
-
-
-def test_initialization_with_invalid_data():
-    # Test initialization of PyMFBase instance with invalid data
-    with pytest.raises(ValueError):
-        PyMFBase(rng.random((10, 20, 30)), num_bases=4)
+    with pytest.raises(NotImplementedError):
+        PyMFBase(sample_data, num_bases=4)
